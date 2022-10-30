1782fd46609e725ea2e605a1a5df66f4c9f03593
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-30 21:28:47 +0300

    finished assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,14 @@
 # Write your code here
+require 'pry'
+def reverse_each_word(sentence)
+    #split sentence into words
+    words = sentence.split(" ")  
+    #loop throught the array of words and reversing each word
+    reversed_words = []
+    words.each do |word|
+        reversed_words << word.reverse
+    end    
+    reversed_words.join(" ")
+end
+binding.pry
+0
